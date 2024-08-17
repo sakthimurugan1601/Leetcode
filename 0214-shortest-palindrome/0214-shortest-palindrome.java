1bf34a5901ffc9801e174fcf3bb3a082a1d69b45
sakthimurugan1601 <148802129+sakthimurugan1601@users.noreply.github.com>
2024-08-17 15:31:09 +0530

    Time: 152 ms (29%), Space: 45.8 MB (5.22%) - LeetHub

--- 0214-shortest-palindrome/0214-shortest-palindrome.java
+++ 0214-shortest-palindrome/0214-shortest-palindrome.java
@@ -1,35 +1,25 @@
-public class Solution {
+class Solution {
     public String shortestPalindrome(String s) {
-        String rev_s = new StringBuilder(s).reverse().toString();
-        String new_s = s + "#" + rev_s;
+        int n = s.length();
+        char[] ch = s.toCharArray(); 
         
-        int n = new_s.length();
-        int[] lps = new int[n];  // Longest prefix suffix array (KMP table)
-
-        // Build the KMP table
-        for (int i = 1; i < n; i++) {
-            int j = lps[i - 1];
-            
-            while (j > 0 && new_s.charAt(i) != new_s.charAt(j)) {
-                j = lps[j - 1];
+        String str = ""; 
+        for (int i = n - 1; i >= 0; i--) {  
+            boolean isPalindrome = true;  
+            for (int j = 0; j <= i; j++) { 
+                if (ch[j] != ch[i - j]) {
+                    isPalindrome = false;  
+                    break; 
+                }
             }
-            
-            if (new_s.charAt(i) == new_s.charAt(j)) {
-                j++;
+            if (!isPalindrome) {
+                str =  str + ch[i]; 
+            } else {
+                break; 
             }
-            
-            lps[i] = j;
         }
-
-        // Characters to add in front of the original string to form the palindrome
-        String toAdd = rev_s.substring(0, s.length() - lps[n - 1]);
         
-        return toAdd + s;
-    }
-
-    public static void main(String[] args) {
-        Solution solution = new Solution();
-        String s = "aacecaaa";
-        System.out.println(solution.shortestPalindrome(s));  // Output: "aaacecaaa"
-    }
+        String a = str + s;
+        return a;
+    }  
 }
